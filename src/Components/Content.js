84d014340d8c770c9d2c646bb613e118b184ea8f
GBao294 <105119912+GBao294@users.noreply.github.com>
2024-01-02 15:59:14 +0700

    Fix Content.js

--- src/Components/Content.js
+++ src/Components/Content.js
@@ -5,7 +5,7 @@ import "../Styles/Content.css";
 function Content() {
   let Images = [];
   //Lấy dữ liệu từ Database
-  var getImage = ref(database, 'ImageInformation/' + 'Image');
+  var getImage = ref(database, 'ImageInformation/Image');
   onValue(getImage, (snapshot) =>{
     snapshot.forEach(childSnapshot => {
         let Key = childSnapshot.key;
